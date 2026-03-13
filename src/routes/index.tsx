cd3dbd9d9cae154d6d3a347ee26685520dcdecff
Matt <mhopps108@users.noreply.github.com>
2026-03-12 20:00:02 -0700

    bu

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -22,6 +22,9 @@ function Home() {
       <h3>Welcome Home!!!</h3>
       <p>{data.message}</p>
       <p>{data.myVar}</p>
+
+      <video src="https://1fcd37eadd955e21cf41ee5b6586ddbc.r2.cloudflarestorage.com/baseball/This%20post%20repurposes%20a%202022%20viral%20video%20of%20Maggie,%20a%20German%20Shepherd%20trained%20as%20a%20'hockey%20goalie.mp4" />
+      <p>Running in Cloudflare-Workers</p>
     </div>
   )
 }
